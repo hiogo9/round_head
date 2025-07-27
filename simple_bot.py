9f8b8a242d5766589874df0e4c16f7ea67cfb9c9
hiogo9 <hiogo9@gmail.com>
2025-07-27 16:45:33 +0300

    add_button

--- simple_bot.py
+++ simple_bot.py
@@ -1,15 +1,20 @@
 import asyncio
-import os 
+import os
 from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
 load_dotenv()
 TOKEN = os.environ["BOT_TOKEN"]
+API_HEYGEN = os.environ["API_HEYGEN"]
+print(TOKEN, API_HEYGEN)
 
-print(TOKEN)
+
+button = KeyboardButton(text="Example button")
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(button)
+
 
 dp = Dispatcher()
 
@@ -20,6 +25,11 @@ async def command_start_handler(message: Message) -> None:
     await message.answer("Prive Andrey ti ochen ymny chel")
 
 
+@dp.message_handler(commands=["start"])
+async def send_welcome(message: types.Message):
+    await message.answer("Hi! Here's a keyboard:", reply_markup=keyboard)
+
+
 # Run the bot
 async def main() -> None:
     bot = Bot(token=TOKEN)
@@ -28,4 +38,3 @@ async def main() -> None:
 
 if __name__ == "__main__":
     asyncio.run(main())
-          
